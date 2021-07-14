f1ba0315d9c9afd7e314b5fe9fc045858df14d7f
pranavxc <pranavxc@gmail.com>
2021-07-14 14:24:59 +0000

    Autorelease from github.com/nocodb/nocodb/packages/noco-doc

---
+++ docs/dist/_nuxt/static/1626272696/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Freleases","\u002Finstall","\u002Fdemos","\u002Fapis\u002Frest\u002Frest-apis-generated","\u002Fapis\u002Fgraphql\u002Fgql-apis-generated"]})
