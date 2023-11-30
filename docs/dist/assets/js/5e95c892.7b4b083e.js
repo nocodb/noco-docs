4f69428c705c7a7597b8bf1fc2199a59eaf0e50f
o1lab <oof1lab@gmail.com>
2023-11-30 07:36:19 +0000

    Autorelease from github.com/nocodb/nocodb/packages/noco-docs

---
+++ docs/dist/assets/js/5e95c892.7b4b083e.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunknoco_docs=self.webpackChunknoco_docs||[]).push([[9661],{34942:(e,s,c)=>{c.r(s),c.d(s,{default:()=>t});c(67294);var r=c(86010),o=c(35463),n=c(23702),u=c(18790),a=c(66945),d=c(85893);function t(e){return(0,d.jsx)(o.FG,{className:(0,r.Z)(n.k.wrapper.docsPages),children:(0,d.jsx)(a.Z,{children:(0,u.H)(e.route.routes)})})}}}]);
