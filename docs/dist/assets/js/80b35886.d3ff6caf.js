f9fd12a13bccbec9f8f83cb04e6947f26192abcc
o1lab <oof1lab@gmail.com>
2024-12-12 13:42:45 +0000

    Autorelease from github.com/nocodb/nocodb/packages/noco-docs

---
+++ docs/dist/assets/js/80b35886.d3ff6caf.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunknoco_docs=self.webpackChunknoco_docs||[]).push([[8515],{28209:s=>{s.exports=JSON.parse('{"label":"Snapshots","permalink":"/tags/snapshots","allTagsPath":"/tags","count":1,"items":[{"id":"bases/snapshots","title":"Base Snapshots \u2601","description":"Procedures to create and manage snapshots","permalink":"/bases/snapshots"}],"unlisted":false}')}}]);
